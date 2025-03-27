0d35a6f528b6ccc856b9e4b565b24e4f6b7b6590
Raphael <raphael@openhdfpv.org>
2025-03-27 14:18:17 +0100

    move kernel to other repo

--- kernel/include/linux/soc/rockchip/rk_fiq_debugger.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef __PLAT_RK_FIQ_DEBUGGER_H
-#define __PLAT_RK_FIQ_DEBUGGER_H
-
-#ifdef CONFIG_FIQ_DEBUGGER_TRUST_ZONE
-void fiq_debugger_fiq(void *regs, u32 cpu);
-#endif
-
-#endif
