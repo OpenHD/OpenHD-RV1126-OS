e361894492a19d0b79bc4b587ccf4d2ab8548689
Raphael <raphael@openhdfpv.org>
2025-03-27 17:12:42 +0100

    Update openhd_part.sh

--- device/rockchip/common/images/oem/oem_ipc/openhd_part.sh
+++ device/rockchip/common/images/oem/oem_ipc/openhd_part.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-FLAGFILE="/etc/.openhd_partition_done"
 DEVICE="/dev/mmcblk1"
+ROOT_MOUNT_SOURCE=$(findmnt -n -o SOURCE /)
 
-if [ ! -f "$FLAGFILE" ]; then
+if [ "$ROOT_MOUNT_SOURCE" != "/dev/root" ]; then
     echo "Running partition GUID modification..."
     sgdisk --partition-guid=6:614e0000-0000-4a59-8000-492b00004044 "$DEVICE" && \
-    touch "$FLAGFILE" && \
     echo "Partition GUID modified successfully!"
     reboot
 else
-    echo "Partition GUID already modified. Skipping..."
+    echo "/ is already mounted from /dev/root. No action needed."
 fi
