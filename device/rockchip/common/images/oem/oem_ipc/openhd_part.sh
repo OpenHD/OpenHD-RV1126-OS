0425977193d72b741c7fd97a50c016f22d39e3e0
Raphael <raphael@openhdfpv.org>
2025-03-27 17:38:43 +0100

    cleanup

--- device/rockchip/common/images/oem/oem_ipc/openhd_part.sh
+++ device/rockchip/common/images/oem/oem_ipc/openhd_part.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+TARGET_GUID="614e0000-0000-4a59-8000-492b00004044"
 DEVICE="/dev/mmcblk1"
-ROOT_MOUNT_SOURCE=$(findmnt -n -o SOURCE /)
 
-if [ "$ROOT_MOUNT_SOURCE" != "/dev/root" ]; then
-    echo "Running partition GUID modification..."
-    sgdisk --partition-guid=6:614e0000-0000-4a59-8000-492b00004044 "$DEVICE" && \
+# Check if any block device partition has the target GUID
+if blkid | grep -q "$TARGET_GUID"; then
+    echo "A partition with GUID $TARGET_GUID is already present. No action needed."
+else
+    echo "No partition with GUID $TARGET_GUID found. Running partition GUID modification..."
+    sgdisk --partition-guid=6:$TARGET_GUID "$DEVICE" && \
     echo "Partition GUID modified successfully!"
     reboot
-else
-    echo "/ is already mounted from /dev/root. No action needed."
 fi
