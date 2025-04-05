0a81c4c27230cbd546ea28d5da1aab3c47c74b25
Raphael <scholle@uni-wuppertal.de>
2025-04-05 11:30:20 +0200

    Update init_OHD.sh

--- init_OHD.sh
+++ init_OHD.sh
@@ -28,6 +28,8 @@ fi
 # Extract the tar.gz file while preserving permissions
 echo "Extracting the file..."
 sudo tar -xpzf buildroot-env-fully-loaded.tar.gz -C /
-gunzip -c buildroot-env-fully-loaded.tar.gz | sudo docker load
+gunzip -c buildroot-env-fully-loaded.tar.gz 
+rm -Rf buildroot-env-fully-loaded.tar.gz
+sudo docker load
 sudo docker run -it --rm buildroot-env:fully-loaded bash -c "/buildroot/build.sh"
 
